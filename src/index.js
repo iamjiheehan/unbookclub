df30d0e9aca3179b1b6fd5a0b1e6e301f4c4068d
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-12 01:25:12 +0900

    index.js 오타수정

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import GlobalStyle from './styled-components/GlobalStyled';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import firebase from './firebase'
 
 
 createRoot(document.getElementById('root')).render(
@@ -14,7 +13,6 @@ createRoot(document.getElementById('root')).render(
       <React.Fragment>
         <GlobalStyle />
         <App />
-        console.log(firebase);
       </React.Fragment>
     </BrowserRouter>
   </React.StrictMode>
