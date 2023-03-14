34529e16e418adb837e1e4ed483f3d05f8f74ff0
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-14 16:44:51 +0900

    Updated navigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import Header from './components/Header';
-import Main from './pages/Main';
-import Board from './pages/Board';
-import Create from './pages/Create';
-import Search from './pages/Search';
-import Footer from './components/Footer';
-import SignIn from './components/SignIn';
-import UserInfo from './components/UserInfo';
+import React, { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Main from "./pages/Main";
+import Board from "./pages/Board";
+import Create from "./pages/Create";
+import Search from "./pages/Search";
+import Footer from "./components/Footer";
+import SignIn from "./components/SignIn";
+import UserInfo from "./components/UserInfo";
 
 import { authService } from "./fBase";
 
@@ -16,40 +16,76 @@ console.log(authService);
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  useEffect(()=>{
-    authService.onAuthStateChanged((user)=>{
+  const [isSignedIn, setIsSignedIn] = useState(authService.currentUser);
+  const [isSignedUp, setIsSignedUp] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(user);
+        setIsSignedIn(user);
       } else {
-        setIsLoggedIn(false);
+        setIsSignedIn(false);
       }
       setInit(true);
-    })
+    });
   }, []);
+
+
   return (
     <div className="App">
       <Header>
         <nav>
           <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/signIn">Sign In</Link></li>
-            <li><Link to="/userInfo">User Info</Link></li>
-            <li><Link to="/board">Board</Link></li>
-            <li><Link to="/create">Create</Link></li>
-            <li><Link to="/search">Search</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signIn">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/userInfo">User Info</Link>
+            </li>
+            <li>
+              <Link to="/board">Board</Link>
+            </li>
+            <li>
+              <Link to="/create">Create</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
           </ul>
         </nav>
       </Header>
       <Routes>
-          <Route path="/" element={ <Main /> } />
-          <Route path="/signIn" element={ <SignIn /> } />
-          <Route path="/board" element={ <Board /> } />
-          <Route path="/create" element={isLoggedIn ? <Create /> : <Navigate to="/signIn" />} />
-          <Route path="/signIn" element={isLoggedIn ? <UserInfo /> : <Navigate to="/signIn" />} />
-          <Route path="/search" element={ <Search /> } />
+        <Route path="/" element={<Main />} />
+        <Route
+          path="/signIn"
+          element={
+            isSignedIn || isSignedUp ? (
+              <UserInfo />
+            ) : (
+              <SignIn setIsSignedUp={setIsSignedUp} />
+            )
+          }
+        />
+        <Route path="/board" element={<Board />} />
+        <Route
+          path="/create"
+          element={isSignedIn ? <Create /> : <Navigate to="/signIn" />}
+        />
+        <Route
+          path="/signIn"
+          element={isSignedIn ? <SignIn /> : <Navigate to="/userInfo" />}
+        />
+        <Route
+          path="/userInfo"
+          element={
+            isSignedIn || isSignedUp ? <UserInfo /> : <Navigate to="/signIn" />
+          }
+        />
+        <Route path="/search" element={<Search />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
