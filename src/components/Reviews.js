5284a4ba2ba37222bb2a9634019b70bf8be98d1c
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-15 16:02:10 +0900

    Updated edit function

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,29 +1,56 @@
 import { dbService } from "fBase";
-import React from "react";
+import React, { useState } from "react";
 import Button from "../styled-components/ButtonStyled";
 
-const Reviews = ({reviewObj, isOwner}) => {
+const Reviews = ({ reviewObj, isOwner }) => {
+    const [editing, setEditing] = useState(false);
+    const [newReview, setNewReview] = useState(reviewObj.review);
     const onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this review?");
-        if(ok) {
-            await dbService.doc(`unBookClub/${reviewObj.id}`).delete();
+        if (ok) {
+        await dbService.doc(`unBookClub/${reviewObj.id}`).delete();
         }
+    };
+
+    const toggleEiditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        console.log(reviewObj, newReview);
+        await dbService.doc(`unBookClub/${reviewObj.id}`).update({
+            review: newReview
+        });
+        setEditing(false);
     }
+    const onChange = (event) => {
+        const {target : {value},}= event;
+        setNewReview(value);
+    };
     return (
+        <>
         <div>
-        <div>
-            <h3> {reviewObj.review} </h3>
-            <h3>글쓴이 : {reviewObj.creatorId}</h3>
-            {isOwner && (
+            {editing ? (
+            <>
+                <form onSubmit = {onSubmit}>
+                    <input type = "text" placeholder="Edit your review" value={newReview} required onChange={onChange} />
+                    <input type="submit" value="Update Review" />
+                </form>
+                <Button>Cancel</Button>
+            </>
+            ) : (
+            <>
+                <h3> {reviewObj.review} </h3>
+                <h3>글쓴이 : {reviewObj.creatorId}</h3>
+                {isOwner && (
                 <>
-                    <Button>수정</Button>
+                    <Button onClick={toggleEiditing}>수정</Button>
                     <Button onClick={onDeleteClick}>삭제</Button>
                 </>
+                )}
+            </>
             )}
         </div>
-    </div>  
-    )
+        </>
+    );
 };
-
 
 export default Reviews;
