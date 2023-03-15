b215d9e24883c12195f8d7b8130f1ba513871f9b
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-16 02:10:48 +0900

    updated styled component

--- src/styled-components/StarStyled.js
+++ src/styled-components/StarStyled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const StarStyled = styled.span`
-    font-size: 2rem; // Increase the font size to make the stars larger
+    font-size: 3rem; // Increase the font size to make the stars larger
     cursor: pointer;
 `;
