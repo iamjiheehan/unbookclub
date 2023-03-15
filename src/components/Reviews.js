dd286fb37be93d39b2e38c6eb5c83f604ff21af7
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-15 14:43:41 +0900

    Updated delete function

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,20 +1,29 @@
+import { dbService } from "fBase";
 import React from "react";
 import Button from "../styled-components/ButtonStyled";
 
-const Reviews = ({reviewObj, isOwner}) => (
-    <div>
+const Reviews = ({reviewObj, isOwner}) => {
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this review?");
+        if(ok) {
+            await dbService.doc(`unBookClub/${reviewObj.id}`).delete();
+        }
+    }
+    return (
+        <div>
         <div>
             <h3> {reviewObj.review} </h3>
             <h3>글쓴이 : {reviewObj.creatorId}</h3>
             {isOwner && (
                 <>
                     <Button>수정</Button>
-                    <Button>삭제</Button>
+                    <Button onClick={onDeleteClick}>삭제</Button>
                 </>
             )}
         </div>
     </div>  
-);
+    )
+};
 
 
 export default Reviews;
