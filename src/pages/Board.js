f4af2c97feb4c893dd0cd69634663dc4cda70ed6
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-17 18:56:24 +0900

    Updated 게시판 레이아웃

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -22,56 +22,58 @@ export default function Board() {
     } = useReviewForm(userObj);
 
     return (
-        <Container>
-        <TextH1 margin="2.5rem 0 auto auto">감상평 게시판</TextH1>
-        <form onSubmit={onSubmit}>
-            <FlexCol>
-            <FlexRow alignItems="baseline">
-                <StarRating totalStars={5} onRatingSelected={onRatingSelected} />
-            </FlexRow>
-            <BoardInput
-                name="bookTitle"
-                value={bookTitle}
-                onChange={onChange}
-                type="text"
-                placeholder="책 제목을 입력해주세요"
-                maxLength={200}
-                margin="auto"
-            />
-            <BoardInput
-                name="bookAuthor"
-                value={bookAuthor}
-                onChange={onChange}
-                type="text"
-                placeholder="작가 이름을 입력해주세요"
-                maxLength={200}
-                margin="auto"
-            />
-            <BoardInput
-                name="inputReview"
-                value={inputReview}
-                onChange={onChange}
-                type="text"
-                placeholder="감상평을 입력해주세요"
-                margin="auto"
-                height="25rem"
-                overflow="auto"
-            />
-            <Input type="submit" value="게시하기" margin="1.5rem auto 0 auto" />
-            </FlexCol>
-        </form>
-        <GridStyled rows="auto" columns="repeat(3,minmax(0,1fr))" margin="3rem">
-            {reviewList.map((review) => (
-            <Reviews
-                key={review.id}
-                reviewObj={review}
-                isOwner={userObj && review.creatorId === userObj.uid}
-                rating={review.selectedRating}
-                bookTitle={review.title}
-                bookAuthor={review.author}
-            />
-            ))}
-        </GridStyled>
-        </Container>
+        <>
+            <Container>
+                <TextH1 margin="2.5rem 0 auto auto">감상평 게시판</TextH1>
+                <form onSubmit={onSubmit}>
+                    <FlexCol>
+                    <FlexRow alignItems="baseline">
+                        <StarRating totalStars={5} onRatingSelected={onRatingSelected} />
+                    </FlexRow>
+                    <BoardInput
+                        name="bookTitle"
+                        value={bookTitle}
+                        onChange={onChange}
+                        type="text"
+                        placeholder="책 제목을 입력해주세요"
+                        maxLength={200}
+                        margin="auto"
+                    />
+                    <BoardInput
+                        name="bookAuthor"
+                        value={bookAuthor}
+                        onChange={onChange}
+                        type="text"
+                        placeholder="작가 이름을 입력해주세요"
+                        maxLength={200}
+                        margin="auto"
+                    />
+                    <BoardInput
+                        name="inputReview"
+                        value={inputReview}
+                        onChange={onChange}
+                        type="text"
+                        placeholder="감상평을 입력해주세요"
+                        margin="auto"
+                        height="25rem"
+                        overflow="auto"
+                    />
+                    <Input type="submit" value="게시하기" margin="1.5rem auto 0 auto" />
+                    </FlexCol>
+                </form>
+            </Container>
+            <GridStyled rows="auto" columns="repeat(3,minmax(0,1fr))" margin="3rem">
+                {reviewList.map((review) => (
+                <Reviews
+                    key={review.id}
+                    reviewObj={review}
+                    isOwner={userObj && review.creatorId === userObj.uid}
+                    rating={review.selectedRating}
+                    bookTitle={review.title}
+                    bookAuthor={review.author}
+                />
+                ))}
+            </GridStyled>
+        </>
     );
 }
