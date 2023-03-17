aeec87b8c456ed17b4e4f004e84dbb50976cbbde
한지희, Jihee Han <hatrix1014@gmail.com>
2023-03-18 02:04:31 +0900

    Updated UserInfo

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -8,7 +8,8 @@ import AuthContext from "../hooks/AuthContext";
 import useFetchReviews from "../hooks/useFetchReviews";
 import useUpdateProfile from "../hooks/useUpdateProfile";
 import { Loading } from "../hooks/useLoading";
-
+import { FlexRow, FlexCol } from "../styled-components/FlexStyled";
+import { Input, BoardInput } from "../styled-components/InputStyled";
 
 export default function UserInfo() {
   const { userObj, setUserObj } = useContext(AuthContext);
@@ -23,20 +24,21 @@ export default function UserInfo() {
 
   return (
     <Container>
-      <BackStyled bgColor="white" padding="3rem" >
-        <TextH1>{userObj.displayName}님 안녕하세요</TextH1>
+      <BackStyled bgColor="white" padding="4rem" >
+        <TextH1><strong>{userObj.displayName}</strong> 님 안녕하세요</TextH1>
         <form onSubmit={onSubmit}>
-          <input onChange={onChange} type="update" placeholder="Update Nickname" value={newDisplayName} />
-          <input type="submit" value="Update Profile" />
+          <FlexCol >
+            <BoardInput margin="2rem auto" width="60%" onChange={onChange} type="update" placeholder="변경된 닉네임은 기존 작성 게시글에도 적용됩니다." value={newDisplayName} />
+            <Input margin="2rem auto 3rem auto" type="submit" value="닉네임 변경하기" />
+          </FlexCol>
+          
         </form>
-        <div>
-          <TextH1>작성한 리뷰 목록</TextH1>
+          <TextH1 padding="0 0 3rem 0">작성한 리뷰 목록</TextH1>
           {loading ? (
             <Loading />
           ) : (
             <ReviewTable reviews={reviews} />
           )}
-        </div>
       </BackStyled>
     </Container>
   );
