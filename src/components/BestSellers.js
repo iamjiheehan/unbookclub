ee9fa0c7ca97827a566452ae0c3100e7d51eaca7
iamjiheehan <hatrix1014@gmail.com>
2023-11-14 17:52:06 +0900

    [Removed ]Unused component

--- src/components/BestSellers.js
+++ src/components/BestSellers.js
@@ -4,8 +4,7 @@ import { addBook } from "store";
 
 import bestSeller from "../data/bestSeller.json";
 import { FlowAniForward } from "../styled-components/AniStyled";
-import { TextH1, TextH2, TextP } from "../styled-components/TextStyled";
-import { ImgStyled } from "../styled-components/ImgStyled";
+
 import {
     BookItemContainer,
     BookInfo,
@@ -13,9 +12,8 @@ import {
 } from "../styled-components/BookStyled";
 import Button from "../styled-components/ButtonStyled";
 import { FaShoppingCart } from "react-icons/fa";
-import { FlexCol, FlexRow } from "styled-components/FlexStyled";
 
-function BestSellers() {
+export function BestSellers() {
     const posts = bestSeller;
 
     let addedBooks = useSelector((state) => state.book);
@@ -87,83 +85,3 @@ function BestSellers() {
         </>
     );
 }
-
-function BestSellersList() {
-    const posts = bestSeller;
-    let addedBooks = useSelector((state) => state.book);
-    const dispatch = useDispatch();
-    const handleAddToCart = (itemId, title, author, coverLargeUrl) => {
-        console.log(
-            "Adding book to cart:",
-            itemId,
-            title,
-            author,
-            coverLargeUrl
-        );
-        const bookToAdd = posts.find((book) => book.itemId === itemId);
-        if (addedBooks.find((book) => book.itemId === itemId)) {
-            console.log("Book already in cart:", itemId);
-            return;
-        }
-        dispatch(addBook({ ...bookToAdd, title, author, coverLargeUrl }));
-        console.log("Book added to cart:", itemId);
-    };
-    return (
-        <>
-            <TextH1 padding="0 0 1rem 0">베스트셀러</TextH1>
-            {posts.map((post) => (
-                <FlexRow
-                    key={post.itemId}
-                    alignItems="center"
-                    justify="flex-start"
-                    margin="2rem"
-                >
-                    <div>
-                        <ImgStyled
-                            src={post.coverLargeUrl}
-                            alt={post.title}
-                            width="250px"
-                            bgShadow="0 0 10px 0 rgba(0, 0, 0, 0.5)"
-                        />
-                    </div>
-                    <div>
-                        <FlexCol
-                            alignItems="flex-start"
-                            margin="2rem"
-                            wrap="nowrap"
-                        >
-                            <TextH2>{post.title}</TextH2>
-                            <TextP padding="1rem 0">{post.author}</TextP>
-                            <TextP textAlign="left">{post.description}</TextP>
-                            <Button
-                                onClick={() =>
-                                    handleAddToCart(
-                                        post.itemId,
-                                        post.title,
-                                        post.author,
-                                        post.coverLargeUrl
-                                    )
-                                }
-                                disabled={addedBooks.some(
-                                    (book) => book.itemId === post.itemId
-                                )}
-                            >
-                                {addedBooks.some(
-                                    (book) => book.itemId === post.itemId
-                                ) ? (
-                                    <p>추가된 도서</p>
-                                ) : (
-                                    <>
-                                        <FaShoppingCart /> 읽을 목록에 추가하기
-                                    </>
-                                )}
-                            </Button>
-                        </FlexCol>
-                    </div>
-                </FlexRow>
-            ))}
-        </>
-    );
-}
-
-export { BestSellers, BestSellersList };
--- src/components/NewBooks.js
+++ src/components/NewBooks.js
@@ -4,18 +4,18 @@ import { addBook } from "store";
 
 import newBooks from "data/newBooks.json";
 import { FlowAniReverse } from "styled-components/AniStyled";
-import { TextH1, TextH2, TextP } from "styled-components/TextStyled";
-import { ImgStyled } from "styled-components/ImgStyled";
-import { FlexCol, FlexRow } from "styled-components/FlexStyled";
+
 import {
     BookItemContainer,
     BookInfo,
     BookImg,
 } from "styled-components/BookStyled";
+
 import Button from "styled-components/ButtonStyled";
+
 import { FaShoppingCart } from "react-icons/fa";
 
-function NewBooks() {
+export function NewBooks() {
     const posts = newBooks;
     let addedBooks = useSelector((state) => state.book);
     const dispatch = useDispatch();
@@ -72,88 +72,6 @@ function NewBooks() {
         </>
     );
 }
-
-function NewBooksList() {
-    const posts = newBooks;
-    let addedBooks = useSelector((state) => state.book);
-    const dispatch = useDispatch();
-
-    const handleAddToCart = (itemId, title, author, coverLargeUrl) => {
-        console.log(
-            "Adding book to cart:",
-            itemId,
-            title,
-            author,
-            coverLargeUrl
-        );
-        const bookToAdd = posts.find((book) => book.itemId === itemId);
-        if (addedBooks.find((book) => book.itemId === itemId)) {
-            console.log("Book already in cart:", itemId);
-            return;
-        }
-        dispatch(addBook({ ...bookToAdd, title, author, coverLargeUrl }));
-        console.log("Book added to cart:", itemId);
-    };
-
-    return (
-        <>
-            <TextH1 padding="0 0 1rem 0">신간도서</TextH1>
-            {posts.map((post) => (
-                <FlexRow
-                    key={post.itemId}
-                    alignItems="center"
-                    justify="flex-start"
-                    margin="2rem"
-                >
-                    <div>
-                        <ImgStyled
-                            src={post.coverLargeUrl}
-                            alt={post.title}
-                            width="250px"
-                            bgShadow="0 0 10px 0 rgba(0, 0, 0, 0.5)"
-                        />
-                    </div>
-                    <div>
-                        <FlexCol
-                            alignItems="flex-start"
-                            margin="2rem"
-                            wrap="nowrap"
-                        >
-                            <TextH2>{post.title}</TextH2>
-                            <TextP padding="1rem 0">{post.author}</TextP>
-                            <TextP textAlign="left">{post.description}</TextP>
-                            <Button
-                                onClick={() =>
-                                    handleAddToCart(
-                                        post.itemId,
-                                        post.title,
-                                        post.author,
-                                        post.coverLargeUrl
-                                    )
-                                }
-                                disabled={addedBooks.some(
-                                    (book) => book.itemId === post.itemId
-                                )}
-                            >
-                                {addedBooks.some(
-                                    (book) => book.itemId === post.itemId
-                                ) ? (
-                                    <p>추가된 도서</p>
-                                ) : (
-                                    <>
-                                        <FaShoppingCart /> 읽을 목록에 추가하기
-                                    </>
-                                )}
-                            </Button>
-                        </FlexCol>
-                    </div>
-                </FlexRow>
-            ))}
-        </>
-    );
-}
-
-export { NewBooks, NewBooksList };
 
 // NewBooks 컴포넌트:
 
